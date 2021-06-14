bf328a94db8258f993cd140a59c002b63907357f
Michael Snoyman <michael@snoyman.com>
2021-06-14 05:55:11 +0300

    Add an example to the derive macro

--- routetype-derive/src/lib.rs
+++ routetype-derive/src/lib.rs
@@ -2,6 +2,24 @@ mod route_parse;
 
 use syn::{parse_macro_input, DeriveInput};
 
+/** Derive the `Route` trait for the given type.
+
+Typically, this is used on an `enum` to provide a `Route` `impl`. You'll need to
+provide `route` attributes on each individual variant, e.g.:
+
+```ignore
+#[derive(Route, Clone, PartialEq, Debug)]
+enum MyRoute {
+    #[route("/")]
+    Home,
+    #[route("css/style.css")]
+    Style,
+    #[route("/hello/{name}")]
+    Hello { name: String },
+}
+```
+
+*/
 #[proc_macro_derive(Route, attributes(route))]
 pub fn derive_route(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
